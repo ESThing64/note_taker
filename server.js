43f5662a73781400f67fdd1b19202ab84fa0afb7
ESThing64 <89550738+ESThing64@users.noreply.github.com>
2021-10-20 16:28:10 -0500

    pushing to continue work on my ipad

--- server.js
+++ server.js
@@ -25,6 +25,73 @@ app.get('/notes', (req, res) =>
 );
 
 
+
+
+
+//get request for a new note
+
+app.get('/api/notes', (req, res) => {
+  res.status(200).json(`${req.method} request recevied to get notes`)
+
+  console.info(`${req.method} yeah. the request was recived for the note thing`)
+})
+
+// post request
+
+app.post('/api/notes', (req, res) => {
+  console.info(`${req.method} <---- this should be post, if it isnt you messed up`);
+
+  const { title, text } = req.body;
+
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuid(),
+    };
+
+    //this shows the json i put in the body.
+    console.log(req.body)
+    console.info(req.rawHeaders);
+
+    //get the existing notes
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.log("🚀 ~ file: server.js ~ line 62 ~ fs.readFile ~ err", err);
+      } else {
+        const parsedNotes = JSON.parse(data)
+        parsedNotes.push(newNote)
+        fs.writeFile(
+          './db/db.json',
+          JSON.stringify(parsedNotes, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Your note has been added!')
+        )
+      }
+
+    })
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    }
+    console.log("🚀 ~ file: server.js ~ line 82 ~ app.post ~ response", response)
+
+    res.status(201).json(response);
+  } else {
+    res.status(500).json("hmmmm That didn't work")
+  }
+
+
+});
+
+
+
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
